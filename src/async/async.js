281ffdc60bbde093db213b79e6fe0efd122745c9
Jorgecas71 <jorgecas71@gmail.com>
2024-06-04 22:02:22 +0200

    added exe async

---
+++ src/async/async.js
@@ -0,0 +1,22 @@
+
+// recuperar los datos
+// imprimir en consola los títulos
+
+const path = '/src/async/data.json'
+ 
+
+ async function getData() {
+   const response = await fetch(path)
+   const data = await response.json()
+   console.log(data);
+ }
+  
+ getData()
+
+ async function renderDataInConsole(data) {
+ for (let index = 0; index < data.results; index++) {
+  console.log((data.results[index].title))
+}
+}
+
+getData()
