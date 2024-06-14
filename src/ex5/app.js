af970d66fb6669aa6c47c02440f73f1e6bef2e74
Jorgecas71 <jorgecas71@gmail.com>
2024-06-14 18:51:47 +0200

    added base ex5 and ex6

---
+++ src/ex5/app.js
@@ -0,0 +1,13 @@
+let a = 8
+let b = 2
+
+function numeroMayor(a, b) {
+    if (a > b) {
+        console.log("El número mayor es " + a);
+    } else if (b > a) {
+        console.log("El número mayor es " + b);
+    } else {
+        console.log("Los números son iguales");
+    }
+}
+
---
+++ src/ex6/app.js
@@ -0,0 +1,3 @@
+let a = 3
+let b = 12
+let c = 4
